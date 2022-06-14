f4075dda36d5a441cd7f0dcd98d91fb8fe6a0f40
Diana Zawislak <diana.zawislak@icloud.com>
2022-06-13 23:15:12 -0400

    some fixes

--- tests/test_task5.py
+++ tests/test_task5.py
@@ -1,8 +1,22 @@
 """Task 5"""
 
+import json
+import logging
+import app
+
 
 def test_task5():
     """Signup for another API and write your own test to verify the data you receive"""
-    # pylint: unused-import, disable=unused-argument, comparison-with-itself, singleton-comparison
+    # pylint: disable=unused-argument, comparison-with-itself, singleton-comparison
 
-    assert True
+    response_logger = logging.getLogger("netflix_api_response")
+    endpoint = "/search/titles?order_by=date&type=movie"
+
+    response = app.netflix_api_request(endpoint)
+    python_object = json.loads(response)
+    response_logger.info(python_object)
+
+    movies = python_object
+    response_logger.info(movies)
+
+    assert len(movies) > 0
